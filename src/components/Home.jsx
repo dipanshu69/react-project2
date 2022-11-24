bead14a71a13e770d49f8285b1819d114a8486d9
Dipanshu <deepanshu.patil69@gmail.com>
2022-11-25 01:29:49 +0530

    Header Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Heading, Image } from '@chakra-ui/react';
+import { Box, Container, Heading, Image, Stack, Text } from '@chakra-ui/react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import img1 from '../assets/1.jpg';
@@ -8,26 +8,94 @@ import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
 import img5 from '../assets/5.png';
 
-const MyCarousel = () => {
-  <Carousel
-    autoPlay
-    infiniteLoop
-    interval={1000}
-    showArrows={false}
-    showStatus={false}
-    showThumbs={false}
-  ></Carousel>;
+const headingOptions = {
+  pos: 'absolute',
+  left: '50%',
+  top: '50%',
+  transform: 'translate(-50%, -50%)',
+  textTransform: 'uppercase',
+  p: '4',
+  size: '4xl',
 };
 
 function Home() {
   return (
-    <Box w="full" h={'100vh'}>
-      <Image src={img1} />
-      <Heading bgColor={'blackAlpha.600'} color={'white'}>
-        Watch The Future
-      </Heading>
+    <Box>
+      <MyCarousel />
+      <Container maxH={'100vh'} maxW={'container.xl'} p="16">
+        <Heading
+          textTransform={'uppercase'}
+          m="auto"
+          w={'fit-content'}
+          borderBottom={'2px solid'}
+        >
+          Services
+        </Heading>
+        <Stack
+          h={'full'}
+          p={'4'}
+          alignItems={'center'}
+          direction={['column', 'row']}
+        >
+          <Image src={img5} h={['40', '400']} filter={'hue-rotate(-130deg)'} />
+          <Text letterSpacing={'widest'} lineHeight={'190%'} p={["4", "16"]} textAlign={"center"}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad nemo
+            tempora deleniti quod, est eos veniam autem asperiores rem, nam
+            laudantium ut, similique ducimus amet sequi et voluptas iusto rerum
+            at aliquid numquam corporis ipsa reprehenderit? Porro iusto magnam,
+            sit vero deleniti, non nemo rem accusamus neque, quibusdam ab.
+            Libero, reprehenderit recusandae labore eaque, aspernatur aliquid
+            modi soluta ullam voluptatum unde repellat in molestias quod?
+            Doloribus ratione illum quisquam earum consequuntur nam voluptatem,
+            velit culpa maiores. Inventore esse illum excepturi dolores est
+            natus aliquid maiores sunt omnis deserunt accusantium amet enim eos
+            adipisci minima hic, nam provident quo aperiam quasi vel?
+            Dignissimos accusantium aliquid aut vel explicabo voluptatum
+            molestias fugit harum reprehenderit incidunt. Quis cumque explicabo
+            omnis officiis autem dolorum!
+          </Text>
+        </Stack>
+      </Container>
     </Box>
   );
 }
+
+const MyCarousel = () => {
+  return (
+    <Carousel
+      autoPlay
+      infiniteLoop
+      interval={2000}
+      showArrows={false}
+      showStatus={false}
+      showThumbs={false}
+    >
+      <Box w="full" h={'100vh'}>
+        <Image src={img1} />
+        <Heading bgColor={'blackAlpha.600'} color={'white'} {...headingOptions}>
+          Watch The Future
+        </Heading>
+      </Box>
+      <Box w="full" h={'100vh'}>
+        <Image src={img2} />
+        <Heading bgColor={'whiteAlpha.900'} color={'black'} {...headingOptions}>
+          Future is Gaming
+        </Heading>
+      </Box>
+      <Box w="full" h={'100vh'}>
+        <Image src={img3} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Gaming on Console
+        </Heading>
+      </Box>
+      <Box w="full" h={'100vh'}>
+        <Image src={img4} />
+        <Heading bgColor={'whiteAlpha.600'} color={'black'} {...headingOptions}>
+          Night Life Is Cool
+        </Heading>
+      </Box>
+    </Carousel>
+  );
+};
 
 export default Home;
