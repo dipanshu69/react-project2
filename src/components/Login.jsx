ea81650af2058cc4c2c11cefc362e1dee004bf8e
Dipanshu <deepanshu.patil69@gmail.com>
2022-11-28 00:22:36 +0530

    project finish

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import {
+  Button,
+  Container,
+  Heading,
+  Input,
+  VStack,
+  Text,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+const Login = () => {
+  return (
+    <Container maxW={'container.xl'} h={'100vh'} p={'16'}>
+      <form>
+        <VStack
+          alignItems={'stretch'}
+          spacing={'8'}
+          w={["full", '96']}
+          m={'auto'}
+          my={'16'}
+        >
+          <Heading alignSelf={'center'}>Welcome Back</Heading>
+          <Input
+            placeholder="Email"
+            required
+            type={'email'}
+            focusBorderColor={'purple.500'}
+          />
+          <Input
+            placeholder="password"
+            type={'password'}
+            required
+            focusBorderColor="purple.500"
+          />
+          <Button variant={'link'} alignSelf="flex-end">
+            <Link to={'/forgetpassword'}>Forget Password?</Link>
+          </Button>
+          <Button colorScheme={'purple'} type="submit">
+            Log In
+          </Button>
+          <Text textAlign="right">
+            New User?{" "}
+            <Button variant={'link'} colorScheme="purple">
+              <Link to={'/signup'}>Sign Up</Link>
+            </Button>
+          </Text>
+        </VStack>
+      </form>
+    </Container>
+  );
+};
+
+export default Login;
